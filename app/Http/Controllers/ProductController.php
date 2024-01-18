b4988197668bad2db764c5b642c760b3d01458da
Muskan Aggarwal <139964721+aggarwal0519@users.noreply.github.com>
2024-01-18 14:45:12 +1100

    add the product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Product;
 
 use Illuminate\Http\Request;
 
@@ -14,5 +15,20 @@ class ProductController extends Controller
 
     public function create(){
         return view('products.create');
+    }
+    public function store(Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'price' => 'required|decimal:2',
+            'description' => 'required|string',
+            'feature_image' => 'required|string',
+            'gallery_image' => 'required|string',
+            'shipping_cost' => 'nullable',
+            'product_status' => 'nullable',
+        ]);
+
+        $newProduct = Product::create($data);
+
+        return redirect(route('product.index'));
     }
 }
